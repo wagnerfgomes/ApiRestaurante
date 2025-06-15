decdde18cbe187262e580c05d00ff79b083b4684
Wagner Felipe <wagnerfelipe4545@gmail.com>
2025-06-15 16:38:28 -0300

    :wrench: chore: Configurando e organizando projeto

---
+++ src/test/java/br/com/projetos/apirestaurante/ApiRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetos.apirestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestauranteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
