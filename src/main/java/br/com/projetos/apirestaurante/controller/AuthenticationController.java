8b643b0ff469b3f44ff7d3acb599fc8a591d5211
Wagner Felipe <wagnerfelipe4545@gmail.com>
2025-07-02 23:51:14 -0300

    :books: docs: Documentando o endpoint de login. backlog: 4

--- src/main/java/br/com/projetos/apirestaurante/controller/AuthenticationController.java
+++ src/main/java/br/com/projetos/apirestaurante/controller/AuthenticationController.java
@@ -2,7 +2,11 @@ package br.com.projetos.apirestaurante.controller;
 
 import br.com.projetos.apirestaurante.DTOs.LoginDto;
 import br.com.projetos.apirestaurante.DTOs.TokenResponseDto;
+import br.com.projetos.apirestaurante.service.ValidateLogin;
 import br.com.projetos.apirestaurante.useCases.LoginUser;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +23,16 @@ public class AuthenticationController {
     @Autowired
     private LoginUser loginUser;
 
+    @Autowired
+    private ValidateLogin validateLogin;
+
+    @Operation(description = "Endpoint for login of users")
+    @ApiResponses(
+            @ApiResponse(
+                    description = "user is logged in and receives a token",
+                    responseCode = "200"
+            )
+    )
     @PostMapping("/login")
     public ResponseEntity<TokenResponseDto> login(@RequestBody LoginDto dto){
         var token = loginUser.authenticate(dto);
