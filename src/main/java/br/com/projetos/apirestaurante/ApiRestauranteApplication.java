3cdb999c813cbd4ba4878f6559de7907d4b774f6
Wagner Felipe <wagnerfelipe4545@gmail.com>
2025-07-02 21:52:10 -0300

    :heavy_plus_sign: build: Configurando e Adicionando Swagger

--- src/main/java/br/com/projetos/apirestaurante/ApiRestauranteApplication.java
+++ src/main/java/br/com/projetos/apirestaurante/ApiRestauranteApplication.java
@@ -1,9 +1,12 @@
 package br.com.projetos.apirestaurante;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API de Gestão de Restaurante", version="1", description="Api para gerenciamento de restaurante"))
 public class ApiRestauranteApplication {
 
     public static void main(String[] args) {
